be2b255b774104ff1ae828a5f5be07cb153eef1d
r0wl <83247404+r0wl@users.noreply.github.com>
2023-10-07 21:40:03 -0400

    Update FCommand.java

--- src/life/steeze/hcfplus/FCommand.java
+++ src/life/steeze/hcfplus/FCommand.java
@@ -14,27 +14,6 @@ import java.util.Map;
 
 public class FCommand implements CommandExecutor {
 
-    private final String[] help = {(ChatColor.WHITE + "---===" + ChatColor.YELLOW + " Factions Help " + ChatColor.WHITE + "===---"),
-            (ChatColor.WHITE + "/f create <name>" + ChatColor.YELLOW + " - Create a faction"),
-            (ChatColor.WHITE + "/f desc <your description here>" + ChatColor.YELLOW + " - Tell everyone what you're about"),
-            (ChatColor.WHITE + "/f invite <name>" + ChatColor.YELLOW + " - Invite your friends"),
-            (ChatColor.WHITE + "/f kick <name>" + ChatColor.YELLOW + " - Kick someone"),
-            (ChatColor.WHITE + "/f setleader <name>" + ChatColor.YELLOW + " - Relinquish your power"),
-            (ChatColor.WHITE + "/f leave" + ChatColor.YELLOW + " - Leave your faction"),
-            (ChatColor.WHITE + "/f info [name]" + ChatColor.YELLOW + " - View a faction's info"),
-            (ChatColor.WHITE + "/f setcolor" + ChatColor.YELLOW + " - Set your faction color"),
-            (ChatColor.WHITE + "/f list" + ChatColor.YELLOW + " - View all factions"),
-            (ChatColor.WHITE + "/f sethome" + ChatColor.YELLOW + " - Set your faction's home"),
-            (ChatColor.WHITE + "/f home" + ChatColor.YELLOW + " - Go to your faction's home"),
-            (ChatColor.WHITE + "/f claim" + ChatColor.YELLOW + " - Obtain a wand to make your faction's claim"),
-            (ChatColor.WHITE + "/f unclaim" + ChatColor.YELLOW + " - Undo your claim"),
-            (ChatColor.WHITE + "/f notify <message>" + ChatColor.YELLOW + " - Send a message to your teammates")},
-
-    adminHelp = {
-            (ChatColor.WHITE + "/f reload" + ChatColor.YELLOW + " - Reload config.yml"),
-            (ChatColor.WHITE + "/f version" + ChatColor.YELLOW + " - Check plugin version"),
-            (ChatColor.WHITE + "/f setdtr <faction/player> <DTR>" + ChatColor.YELLOW + " - Set a faction's DTR")
-    };
 
     private void sendHelp(Player commandSender) {
         for(Map.Entry<String, SubCommand> e : subCommandMap.entrySet()){
